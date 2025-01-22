d181e80d1064ca742dcb68fcb8678c7c4e115a66
Ariel Nathan <nathanariel17@gmail.com>
2025-01-22 12:12:28 -0500

    Update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Container from "@/components/container";
 export default function Home() {
   return (
     <Container className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+      <div className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -50,7 +50,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-      </main>
+      </div>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
