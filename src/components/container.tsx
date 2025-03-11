5ddd8d1bbb0630260f5b7964d47075f690a27603
Ariel Nathan <nathanariel17@gmail.com>
2025-03-11 13:53:28 -0400

    Update dependencies and container utility

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -5,8 +5,6 @@ import { cn } from "@/lib/utils"
 const Container: React.FC<React.ComponentProps<"div">> = ({
   className,
   ...props
-}) => (
-  <div className={cn("container flex grow flex-col", className)} {...props} />
-)
+}) => <div className={cn("container grow", className)} {...props} />
 
 export default Container
